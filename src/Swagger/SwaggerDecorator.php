60351070505e6007fc0d5e548aa5edbb3da0101b
kodmit <alexandre.bailly@live.fr>
2019-04-10 18:56:26 +0200

    add decorator for swagger

---
+++ src/Swagger/SwaggerDecorator.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Swagger;
+
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+final class SwaggerDecorator implements NormalizerInterface
+{
+    private $decorated;
+
+    public function __construct(NormalizerInterface $decorated)
+    {
+        $this->decorated = $decorated;
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $docs = $this->decorated->normalize($object, $format, $context);
+        $docs['info']['title'] = 'My Api Foo';
+        return $docs;
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        return $this->decorated->supportsNormalization($data, $format);
+    }
+}
